c8a1b58614a6c32d6d23ca642310478673e26c18
ilfizamutiara <ilfiza.mutiara@gmail.com>
2022-02-27 06:56:49 +0700

    com

--- app/Models/kurir.php
+++ app/Models/kurir.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class kurir extends Model
+class Kurir extends Model
 {
     protected $fillable = ['id','kurir'];
 }
--- app/Models/percetakan.php
+++ app/Models/percetakan.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class percetakan extends Model
+class Percetakan extends Model
 {
    // use HasFactory;
    protected $primaryKey = 'id_percetakan';
